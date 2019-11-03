5dac0eb2e29b584dbda1898989eb778a2b217e97
장성범 <iwsl1234@naver.com>
2019-11-03 15:04:07 +0900

    상세 정보 통신

--- MYPOLICY/app/src/main/java/com/example/mypolicy/DetailPolicyActivity.java
+++ MYPOLICY/app/src/main/java/com/example/mypolicy/DetailPolicyActivity.java
@@ -8,9 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mypolicy.adapter.PolicyAdapter;
 import com.example.mypolicy.model.Policy;
+import com.example.mypolicy.model.Review;
 import com.example.mypolicy.service.IApiService;
 import com.example.mypolicy.service.RestClient;
 import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -25,19 +29,29 @@ public class DetailPolicyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_policy);
+
         intent=getIntent();
         position=intent.getIntExtra("position",1);
+
         Log.d("값",""+intent.getIntExtra("position",1));
+        Log.d("값2",""+intent.getStringExtra("Title"));
+        Log.d("값2",""+intent.getStringExtra("applyStart"));
+//        Log.d("값2",""+intent.getStringExtra("applyEnd"));
+//        Log.d("값2",""+intent.getStringExtra("uri"));
+//        Log.d("값2",""+intent.getStringExtra("StartAge"));
+//        Log.d("값2",""+intent.getStringExtra("detailContent"));
+//        Log.d("값2",""+intent.getStringExtra("location"));
         IApiService iApiService=new RestClient("http://49.236.136.213:3000/").getApiService();
         Call<ArrayList<Policy>> call=iApiService.showselectedPolicy(position);
+        Call<ArrayList<Review>> reviewcall=iApiService.showReview(position);
 
         try{
             call.enqueue(new Callback<ArrayList<Policy>>() {
                 @Override
                 public void onResponse(Call<ArrayList<Policy>> call, Response<ArrayList<Policy>> response) {
                     String tmp=new Gson().toJson(response.body());
+
                     Log.d("각각정보",""+position+tmp);
-                    setUI();
                 }
 
                 private void setUI() {
@@ -52,6 +66,31 @@ public class DetailPolicyActivity extends AppCompatActivity {
         }catch(Exception e)
         {
             e.printStackTrace();
+        }
+
+        try{
+            reviewcall.enqueue(new Callback<ArrayList<Review>>() {
+                @Override
+                public void onResponse(Call<ArrayList<Review>> call, Response<ArrayList<Review>> response) {
+                    String tmp=new Gson().toJson(response.body());
+                    try {
+                        JSONArray jsonArray = new JSONArray(tmp);
+                        Log.d("리뷰길이",""+jsonArray.length());
+                    }catch (JSONException j)
+                    {
+                        j.printStackTrace();
+                    }
+                    Log.d("리뷰값",""+position+tmp);
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<Review>> call, Throwable t) {
+
+                }
+            });
+        }catch (Exception t)
+        {
+            t.printStackTrace();
         }
     }
 }
