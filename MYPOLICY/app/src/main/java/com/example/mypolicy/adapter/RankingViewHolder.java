0d773f50a2ba78a66a219e69a8c68a4dcb786140
장성범 <iwsl1234@naver.com>
2019-11-28 20:31:27 +0900

    asdf

--- MYPOLICY/app/src/main/java/com/example/mypolicy/adapter/RankingViewHolder.java
+++ MYPOLICY/app/src/main/java/com/example/mypolicy/adapter/RankingViewHolder.java
@@ -1,11 +1,23 @@
 package com.example.mypolicy.adapter;
 
+import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-//public class RankingViewHolder extends RecyclerView.ViewHolder {
-//    public TextView p_code;
-//    public TextView title;
-//    public TextView views;
-//}
+import com.example.mypolicy.R;
+
+public class RankingViewHolder extends RecyclerView.ViewHolder {
+    public TextView p_code;
+    public TextView title;
+    public TextView views;
+
+    public RankingViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+        p_code=(TextView)itemView.findViewById(R.id.tv_ranking_pcode);
+        title=(TextView)itemView.findViewById(R.id.tv_ranking_title);
+
+    }
+}
