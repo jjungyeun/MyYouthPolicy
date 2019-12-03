f472e17ebad508fbb02302d8d488225e88f5846f
장성범 <iwsl1234@naver.com>
2019-12-03 17:35:56 +0900

    af

--- MYPOLICY/app/src/main/java/com/example/mypolicy/ProfileActivity.java
+++ MYPOLICY/app/src/main/java/com/example/mypolicy/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,6 +94,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             @Override
             public void onClick(View view) {
                 rd.callFunction();
+
             }
         });
 
